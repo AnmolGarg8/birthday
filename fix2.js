220c0b14ea4724738b719905c2d5e7af97811fe8
AnmolGarg8 <anmolgarg1605@gmail.com>
2026-05-27 08:35:32 +0530

    Replaced entire birthday website with premium Stay Tuned placeholder page

--- fix2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.split('<div class="cake-slice-left">??</div>').join('<div class="cake-slice-left">🍰</div>');
-html = html.split('<div class="cake-slice-right">??</div>').join('<div class="cake-slice-right">🍰</div>');
-html = html.split('<div class="knife-emoji">??</div>').join('<div class="knife-emoji">🔪</div>');
-html = html.split('<div class="full-cake">??</div>').join('<div class="full-cake">🎂</div>');
-fs.writeFileSync('index.html', html, 'utf8');
